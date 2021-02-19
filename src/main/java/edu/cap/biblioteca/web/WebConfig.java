6fdebe3db53ecd4093b783178f3f1668a7491e75
Romera Matias <romeramatias0@gmail.com>
2021-02-19 14:38:37 -0300

    Edit encoding, index messages

--- src/main/java/edu/cap/biblioteca/web/WebConfig.java
+++ src/main/java/edu/cap/biblioteca/web/WebConfig.java
@@ -6,8 +6,10 @@ package edu.cap.biblioteca.web;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -18,12 +20,21 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+
 	@Bean
 	public LocaleResolver localeResolver() {
 		var sessionLocaleResolver = new SessionLocaleResolver();
 		sessionLocaleResolver.setDefaultLocale(new Locale("es"));
 		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(10);
+		return messageSource;
 	}
 
 	@Bean
